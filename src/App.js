b32d3d3b03ef33d7925f3dc15615f1e065cfd8fa
TechKumarNitish <rskban15a2015@gmail.com>
2025-06-07 17:42:44 +0530

    added toggling logic for snap by 40px

--- src/App.js
+++ src/App.js
@@ -8,14 +8,19 @@ import DownloadButton from './components/DownloadButton';
 function App() {
   const stageRef = useRef();
   const [stickers, setStickers] = useState([]);
+  const [snapEnabled, setSnapEnabled] = useState(true);
+
+  const snapChangeHandler = () => {
+    setSnapEnabled(prevState => !prevState)
+  }
 
   const handleAddSticker = (src) => {
     const x = Math.floor(Math.random() * (400 - 200 + 1)) + 200;  // 200–400
     const y = Math.floor(Math.random() * (300 - 200 + 1)) + 200;  // 200–300  
     const newSticker = {
       id: Date.now(),
-      x:x,
-      y:y,
+      x: x,
+      y: y,
       src
     };
     setStickers([...stickers, newSticker]);
@@ -26,10 +31,18 @@ function App() {
   };
 
   const handleDragEnd = (id, x, y) => {
-    const snappedX = Math.round(x / 40) * 40;
-    const snappedY = Math.round(y / 40) * 40;
+
+    console.log("prev: ", x, y)
+
+    if (snapEnabled) {
+      x = Math.round(x / 40) * 40;
+      y = Math.round(y / 40) * 40;
+    }
+
+    console.log("now: ", x, y)
+
     setStickers(stickers.map(s =>
-      s.id === id ? { ...s, x: snappedX, y: snappedY } : s
+      s.id === id ? { ...s, x: x, y: y } : s
     ));
   };
 
@@ -45,19 +58,31 @@ function App() {
           Click the button below to download the canvas as an image.
           Click a sticker to add it to the canvas.
         </p>
-      <div className='sticker-toolbar'>
-        <StickerToolbar onAdd={handleAddSticker} />
-        <div style={{ marginLeft: 20 }}>
-          <CanvasBoard
-            stageRef={stageRef}
-            stickers={stickers}
-            onDelete={handleDeleteSticker}
-            onDragEnd={handleDragEnd}
+
+        <div className='snap-box-container'>
+          <input type='checkbox'
+            className='snap-check-box'
+            checked={snapEnabled}
+            onChange={snapChangeHandler}
           />
-          <DownloadButton stageRef={stageRef} />
+          <label className='snap-check-box-label'>
+            Snap to Grid (40px)
+          </label>
+        </div>
+
+        <div className='sticker-toolbar'>
+          <StickerToolbar onAdd={handleAddSticker} />
+          <div style={{ marginLeft: 20 }}>
+            <CanvasBoard
+              stageRef={stageRef}
+              stickers={stickers}
+              onDelete={handleDeleteSticker}
+              onDragEnd={handleDragEnd}
+            />
+            <DownloadButton stageRef={stageRef} />
+          </div>
         </div>
       </div>
-    </div>
     </div >
   );
 }
